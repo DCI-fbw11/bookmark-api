f93de3ee95331b5c6b1537f73ea2359725021855
Bilie <bilianavaleva@gmail.com>
2019-04-25 15:20:53 +0200

    Save bookmark to db via mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const logger = require("morgan")
+const mongoose = require("mongoose")
 
 const PORT = process.env.PORT || 4000
 
@@ -7,6 +8,16 @@ const indexRouter = require("./routes/index")
 const apiRoutes = require("./routes/api")
 
 const app = express()
+
+mongoose.connect("mongodb://localhost/bookmarks", {
+  useNewUrlParser: true
+})
+.then(() => {
+  console.log('Connected to Mongo')
+})
+.catch((err) => {
+  console.error('Could not connect, ', err)
+})
 
 app.use(logger("dev"))
 app.use(express.json())
