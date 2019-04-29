25259291af30a0aea83173db6ecd51ef59061d94
schkiho <schkiho@yahoo.de>
2019-04-29 12:08:42 +0200

    change update to mongoose

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -1,15 +1,6 @@
 const db = require("../db")
-<<<<<<< HEAD
-const createError = require("../tools/createError")
-const { noBookmarkFound, noIDDefined } = require("../tools/errorMessages")
-=======
 const createError = require("../helpers/createError")
-const {
-  noBookmarkFound,
-  noIDDefined,
-  noURLDefined
-} = require("../helpers/errorMessages")
->>>>>>> f5d807e960c139c0ea602cbe28981d480e6b28c1
+const { noBookmarkFound, noIDDefined } = require("../helpers/errorMessages")
 const Bookmark = require("../models/bookmark")
 
 module.exports = {
@@ -57,21 +48,63 @@ module.exports = {
   },
 
   updateBookmarkById: (req, res, next) => {
-    const { bookmark } = Bookmark.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
-    })
-
-    if (!bookmark)
+    const { id } = req.params
+    const updateBookmark = req.body
+    console.log(req.params)
+    if (!id)
       return res
         .status(404)
         .send("The bookmark with the given ID was not found.")
 
-    res.locals.response = Object.assign({}, res.locals.response || {}, {
-      bookmark: bookmark
-    })
-
-    next()
+    Bookmark.updateOne({ _id: id }, updateBookmark, { new: true })
+      .then(updatedBookmark => {
+        res.locals.response = Object.assign({}, res.locals.response || {}, {
+          bookmark: updatedBookmark
+        })
+      })
+      .catch(err => {
+        console.error(err)
+      })
+      .finally(() => {
+        next()
+      })
   },
+
+  /*====================================================*
+
+  *    PUT REQUEST TO http://localhost:4000/api/bookmarks/5cc56589f2d7821759d5ad45
+
+      {
+	
+	      "title": "Boomark Title",
+	      "shortDescription": "Bookmark shortdescription",
+	      "url": "yahoo.com",
+	      "tag": ["fistTag", "secondtag"]
+
+
+      }
+  *
+  *    UPDATE BOOKMARK BY ID
+  */
+
+  //  updateBookmarkById: async (req, res) => {
+  //   const { bookmarkId } = req.params
+  //   const updateBookmark = req.body
+
+  //   try {
+  //     await Bookmark.updateOne({_id: bookmarkId}, updateBookmark, { new:true })
+  //     res.locals.response = Object.assign({}, res.locals.response || {}, {
+  //     bookmark: res.status(200).json({msg: `Bookmark with the id ${bookmarkId} is updated`})
+  //   })
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  //   finally(){
+  //     return next()
+  //   }
+
+  // },
+  /*====================================================*/
 
   deleteBookmarkById: (req, res, next) => {
     const { id } = req.params
