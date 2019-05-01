5f6cff2e07a482cc79ecfded93cf25aaded5e61a
Martyna Krysinska <m.krysinska@tutanota.com>
2019-05-01 23:52:51 +0200

    set the error message and the http response status

--- controller/tag.js
+++ controller/tag.js
@@ -1,5 +1,6 @@
 const Tag = require("../models/tag")
-
+const createError = require("../helpers/createError")
+const errorMessages = require("../helpers/errorMessages")
 module.exports = {
   createTag: (req, res, next) => {
     const newTag = new Tag(req.body)
@@ -11,7 +12,10 @@ module.exports = {
         })
       })
       .catch(err => {
-        console.error(err)
+        if (err.name === "ValidationError") {
+          err.statusCode = 400
+        } // move validation to schema later to avoid duplicating on each controller?
+        next(err)
       })
       .finally(() => next())
   }
