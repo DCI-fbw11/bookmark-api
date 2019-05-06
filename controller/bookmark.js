0cdc5a581f1f7699105047f837642780009720e4
Quoc-Khanh Truong <khanh.truong@hotmail.de>
2019-05-06 15:19:25 +0200

    added updatedAt

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -59,7 +59,10 @@ module.exports = {
 
   updateBookmarkById: (req, res, next) => {
     const { id } = req.params
-    const updateBookmark = req.body
+    //const updateBookmark = req.body
+    const updateBookmark = Object.assign({}, req.body, {
+      updatedAt: Date.now()
+    })
 
     Bookmark.findOneAndUpdate({ _id: id }, updateBookmark, { new: true })
       .then(updatedBookmark => {
