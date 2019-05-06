e092ebff3248d4f404b2024c45d5ca239f7d38d0
Bilie <bilianavaleva@gmail.com>
2019-05-06 10:03:14 +0200

    Remove unneeded code

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -69,43 +69,7 @@ module.exports = {
         next()
       })
   },
-
-  /*====================================================*
-
-  *    PUT REQUEST TO http://localhost:4000/api/bookmarks/5cc56589f2d7821759d5ad45
-
-      {
-	
-	      "title": "Boomark Title",
-	      "shortDescription": "Bookmark shortdescription",
-	      "url": "yahoo.com",
-	      "tag": ["fistTag", "secondtag"]
-
-
-      }
-  *
-  *    UPDATE BOOKMARK BY ID
-  */
-
-  //  updateBookmarkById: async (req, res) => {
-  //   const { bookmarkId } = req.params
-  //   const updateBookmark = req.body
-
-  //   try {
-  //     await Bookmark.updateOne({_id: bookmarkId}, updateBookmark, { new:true })
-  //     res.locals.response = Object.assign({}, res.locals.response || {}, {
-  //     bookmark: res.status(200).json({msg: `Bookmark with the id ${bookmarkId} is updated`})
-  //   })
-  //   } catch (err) {
-  //     console.error(err)
-  //   }
-  //   finally(){
-  //     return next()
-  //   }
-
-  // },
-  /*====================================================*/
-
+ 
   deleteBookmarkById: (req, res, next) => {
     const { id } = req.params
 
