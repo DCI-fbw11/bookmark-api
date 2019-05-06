331bf95ecb2e0a83e92bd167ad35dfe8384dbf68
Bilie <bilianavaleva@gmail.com>
2019-05-06 10:00:44 +0200

    Remove not needed lines

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const logger = require("morgan")
 const mongoose = require("mongoose")
 
 const PORT = process.env.PORT || 4000
-
-//const indexRouter = require("./routes/index")
 const apiRoutes = require("./routes/api")
-
 const app = express()
 
 mongoose
@@ -22,8 +19,6 @@ mongoose
 
 app.use(logger("dev"))
 app.use(express.json())
-
-// app.use("/", indexRouter)
 app.use("/api", apiRoutes)
 
 app.listen(PORT, console.log("Server is listening on port:", PORT))
