cbcb0013a092188cc849c498679f7400f5c3c1c2
Oliver Weber <oliver@mondough.io>
2019-05-08 14:21:03 +0200

    finish up tests with connection
    We should just use one instance of mongoose to make mongoose.connection work

--- tests/getBookmarks.test.js
+++ tests/getBookmarks.test.js
@@ -1,11 +1,16 @@
 const request = require("supertest")
-const mongoose = require("mongoose")
-const app = require("../app")
+
+const { mongoose } = require("../db/connection")
+const Bookmark = require("../models/bookmark")
+const { app } = require("../app")
 const { apiRoutes } = require("../routes/api")
 
 const apiRoutePrefix = "/api"
 
-afterAll(() => mongoose.disconnect())
+beforeAll(async () => {
+  await mongoose.connection.dropCollection("bookmarks")
+})
+afterAll(done => mongoose.disconnect(done))
 
 describe("GET /bookmarks tests", () => {
   test("Get all bookmarks should respond with status code 200", async () => {
@@ -14,5 +19,25 @@ describe("GET /bookmarks tests", () => {
     )
 
     expect(response.statusCode).toBe(200)
+  })
+
+  test("GET /bookmarks contains right amount of bookmarks", async () => {
+    // add a bookmark here
+    const newBookmarkData = {
+      url: "https://awesomedomain.tld",
+      title: "best bookmark ever"
+    }
+    await new Bookmark(newBookmarkData).save()
+
+    const response = await request(app).get(
+      apiRoutePrefix + apiRoutes.getAllBookmarks
+    )
+
+    const {
+      body: { data }
+    } = response
+
+    expect(data.bookmark.length).toBe(1)
+    expect(data.bookmark[0].url).toEqual(newBookmarkData.url)
   })
 })
