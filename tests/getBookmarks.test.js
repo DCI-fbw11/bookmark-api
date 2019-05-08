6727bfe3f25dfce8d03dd8f10e9215c8308b8b22
Oliver Weber <oliver@mondough.io>
2019-05-08 11:53:30 +0200

    basic getBookmarks test

---
+++ tests/getBookmarks.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest")
+const mongoose = require("mongoose")
+const app = require("../app")
+const { apiRoutes } = require("../routes/api")
+
+const apiRoutePrefix = "/api"
+
+describe("GET /bookmarks tests", () => {
+  test("Get all bookmarks should respond with status code 200", async () => {
+    const response = await request(app).get(
+      apiRoutePrefix + apiRoutes.getAllBookmarks
+    )
+
+    expect(response.statusCode).toBe(200)
+  })
+})
