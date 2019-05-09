1a5baa15fa7b47a65ea9aed590438cc66a4e1ad9
sit79 <stefan.tolksdorf@gmail.com>
2019-05-09 16:59:36 +0200

    error messages as requested

--- helpers/errorMessages.js
+++ helpers/errorMessages.js
@@ -2,5 +2,6 @@ module.exports = {
   noIDDefined: "No ID defined, bookmarks/:id",
   noBookmarks: "No bookmarks currently exist",
   noBookmarkFound: "No bookmark found for id",
-  noURLDefined: "No url to bookmark defined"
+  noURLDefined: "No url to bookmark defined",
+  invalidID: "Invalid ID, please provide a valid ID"
 }
