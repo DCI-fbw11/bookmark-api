3a92bfd3fbbbb53462af7bf6e82c1bc7155cd9f5
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-05-09 14:13:49 +0200

    refactoring code from promises to async/await

--- controller/bookmark.js
+++ controller/bookmark.js
@@ -9,99 +9,94 @@ const {
 const Bookmark = require("../models/bookmark")
 
 module.exports = {
-  getBookmarks: (req, res, next) => {
-    Bookmark.find({})
-      .then(bookmarkList => {
-        if (!bookmarkList) {
-          createError(400, noBookmarks)
-        } else {
-          res.locals.response = Object.assign({}, res.locals.response || {}, {
-            bookmark: bookmarkList
-          })
-        }
-      })
-      .catch(err => next(err))
-      .finally(() => next())
+  getBookmarks: async (req, res, next) => {
+    try {
+      const bookmarkList = await Bookmark.find({})
+      if (!bookmarkList) {
+        createError(400, noBookmarks)
+      } else {
+        res.locals.response = Object.assign({}, res.locals.response || {}, {
+          bookmark: bookmarkList
+        })
+      }
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+    next()
   },
 
-  getBookmarkByID: (req, res, next) => {
+  getBookmarkByID: async (req, res, next) => {
     const { id } = req.params
 
-    Bookmark.findOne({ _id: id })
-      .then(foundBookmark => {
-        if (!foundBookmark) {
-          createError(400, noBookmarkFound)
-        } else {
-          res.locals.response = Object.assign({}, res.locals.response || {}, {
-            bookmark: foundBookmark
-          })
-        }
-      })
-      .catch(err => next(err))
-      .finally(() => next())
+    try {
+      const foundBookmark = await Bookmark.findOne({ _id: id })
+      if (!foundBookmark) {
+        createError(400, noBookmarkFound)
+      } else {
+        res.locals.response = Object.assign({}, res.locals.response || {}, {
+          bookmark: foundBookmark
+        })
+      }
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+    next()
   },
 
-  postBookmark: (req, res, next) => {
+  postBookmark: async (req, res, next) => {
     const newBookmark = new Bookmark(req.body)
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() })
     }
-    newBookmark
-      .save()
-      .then(savedBookmark => {
-        res.locals.response = Object.assign({}, res.locals.response || {}, {
-          bookmark: savedBookmark
-        })
+    try {
+      const savedBookmark = await newBookmark.save()
+      res.locals.response = Object.assign({}, res.locals.response || {}, {
+        bookmark: savedBookmark
       })
-      .catch(err => {
-        next(err)
-      })
-      .finally(() => {
-        next()
-      })
+    } catch (error) {
+      next(error)
+    }
+    next()
   },
 
-  updateBookmarkById: (req, res, next) => {
+  updateBookmarkById: async (req, res, next) => {
     const { id } = req.params
     const updateBookmark = Object.assign({}, req.body, {
       updatedAt: Date.now()
     })
+    try {
+      const updatedBookmark = await Bookmark.findOneAndUpdate(
+        { _id: id },
+        updateBookmark,
+        {
+          runValidators: true
+        }
+      )
 
-    Bookmark.findOneAndUpdate({ _id: id }, updateBookmark, {
-      runValidators: true
-    })
-      .then(updatedBookmark => {
-        res.locals.response = Object.assign({}, res.locals.response || {}, {
-          bookmark: updatedBookmark,
-          message: `Bookmark with id ${id} was updated!`
-        })
+      res.locals.response = Object.assign({}, res.locals.response || {}, {
+        bookmark: updatedBookmark,
+        message: `Bookmark with id ${id} was updated!`
       })
-      .catch(err => {
-        err.message = "Wrong ID please enter a valid ID"
-        next(err)
-      })
-      .finally(() => {
-        next()
-      })
+    } catch (error) {
+      next(error)
+    }
   },
 
-  deleteBookmarkById: (req, res, next) => {
+  deleteBookmarkById: async (req, res, next) => {
     const { id } = req.params
-
-    Bookmark.findByIdAndRemove({ _id: id })
-      .then(deleteBookmark => {
-        res.locals.response = Object.assign({}, res.locals.response || {}, {
-          bookmark: deleteBookmark,
-          message: `Bookmark with id ${id} was deleted!`
-        })
+    try {
+      const deleteBookmark = Bookmark.findByIdAndRemove({ _id: id })
+      res.locals.response = Object.assign({}, res.locals.response || {}, {
+        bookmark: deleteBookmark,
+        message: `Bookmark with id ${id} was deleted!`
       })
-      .catch(err => {
-        next(err)
-      })
-      .finally(() => {
-        next()
-      })
+    } catch (error) {
+      next(error)
+    }
+    next()
   },
 
   batchDeleteBookmarks: (req, res, next) => {
