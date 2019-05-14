ca3d70616635efc43cbe4341f1e3e9e28b1dd00b
Oliver Weber <oliver@mondough.io>
2019-05-14 12:02:39 +0200

    setup now as a deployment strategy

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose")
 mongoose.set("useCreateIndex", true)
-const { NODE_ENV } = process.env
+const { NODE_ENV, DB_PROD_USER, DB_PROD_PASSWORD } = process.env
 
 const dbPaths = {
   development: "bookmarks",
-  test: "bookmarks-test"
+  test: "bookmarks-test",
+  production: "bookmark-api-prod"
+}
+
+let connectionString
+
+if (NODE_ENV === "production") {
+  connectionString = `mongodb://${DB_PROD_USER}:${DB_PROD_PASSWORD}@cluster0-shard-00-00-u0h0i.mongodb.net:27017,cluster0-shard-00-01-u0h0i.mongodb.net:27017,cluster0-shard-00-02-u0h0i.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin`
+} else {
+  connectionString = "mongodb://127.0.0.1/"
 }
 
 module.exports = {
   mongoose,
   connect: () => {
     mongoose.Promise = Promise
-    return mongoose.connect("mongodb://127.0.0.1/" + dbPaths[NODE_ENV], {
+    return mongoose.connect(connectionString + dbPaths[NODE_ENV], {
       useNewUrlParser: true
     })
   },
