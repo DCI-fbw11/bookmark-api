d978b08fd964fb31896c239178b0a7c41bb6b58e
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-05-15 14:19:30 +0200

    changing help wording

--- helpers/dateParser.js
+++ helpers/dateParser.js
@@ -1,6 +1,6 @@
 // This function parses the date format from the user
 // user input query: /bookmarks?startDate=2018.12.01&endDate=2019.05.15
-// parsed result = 2018,12,1 2019,5,15 which is needed for new Date() in controller
+// parsed result = 2018,12,1 2019,5,15 which is the required format needed for new Date() in controller
 
 const dateParser = (start, end) => {
   let parseStart = start.replace(/(\.)/g, ",")
