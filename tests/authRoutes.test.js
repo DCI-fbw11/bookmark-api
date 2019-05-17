2fd7683033bdb53f77a47e39a21522d18362dfcb
Gunes <xqwtsz@gmail.com>
2019-05-17 15:00:09 +0200

    Add tests for auth routes (#148)
    :dango:

---
+++ tests/authRoutes.test.js
@@ -0,0 +1,58 @@
+const request = require("supertest")
+const app = require("../app")
+const { mongoose } = require("../db/connection")
+
+const { authRoutes } = require("../routes/auth")
+
+const authRoutePrefix = "/auth"
+
+beforeAll(async () => {
+  await mongoose.connection.on("connected", () => Promise.resolve())
+  await mongoose.connection.dropDatabase()
+})
+
+afterAll(done => mongoose.disconnect(done))
+
+describe("Auth tests", () => {
+  test("Register should respond with status code 200", async done => {
+    const response = await request(app)
+      .post(authRoutePrefix + authRoutes.register)
+      .send({
+        registerData: {
+          username: "testUser",
+          password: "12345678"
+        }
+      })
+
+    expect(response.statusCode).toBe(200)
+    done()
+  })
+
+  test("Login should respond with a token", async done => {
+    const response = await request(app)
+      .post(authRoutePrefix + authRoutes.login)
+      .send({
+        loginData: {
+          username: "testUser",
+          password: "12345678"
+        }
+      })
+
+    expect(response.body.data.token).not.toBeNull()
+    done()
+  })
+
+  test("Login with wrong password should respond with a null token", async done => {
+    const response = await request(app)
+      .post(authRoutePrefix + authRoutes.login)
+      .send({
+        loginData: {
+          username: "testUser",
+          password: "1234asasda5678"
+        }
+      })
+
+    expect(response.body.data.token).toBeNull()
+    done()
+  })
+})
